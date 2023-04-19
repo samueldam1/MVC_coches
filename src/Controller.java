85b6992f0c5b888d425c2108236ab85c89c445e0
Damian Nogueiras <damian@danielcastelao.org>
2023-04-19 14:54:46 +0200

    Initial commit

---
+++ src/Controller.java
@@ -0,0 +1,14 @@
+public class Controller {
+    public static void main(String[] args) {
+        Model miModelo = new Model();
+
+        // Crear tres coches
+
+        miModelo.crearCoche("LaFerrari", "SBC 1234");
+        miModelo.crearCoche("Alpine", "HYU 4567");
+        miModelo.crearCoche("Aston Martin", "FGH 3333");
+
+        Coche ferrari = miModelo.getCoche("SBC 1234");
+        System.out.println("FIN");
+    }
+}
---
+++ src/Model.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Model {
+    static ArrayList<Coche> parking = new ArrayList<>();
+
+    /**
+     * Crea un coche y lo mete en el parking
+     * @param modelo del coche
+     * @param matricula identificador unico
+     * @return el coche creado
+     */
+    public Coche crearCoche(String modelo, String matricula){
+        Coche aux = new Coche(modelo, matricula);
+        parking.add(aux);
+        return aux;
+    }
+
+    /**
+     * Busca coche segun matricula
+     * @param matricula a buscar
+     * @return chche o null si no existe
+     */
+    public Coche getCoche(String matricula){
+        Coche aux = null;
+        for (Coche e: parking) {
+            if (e.matricula.equals(matricula)) {
+                aux = e;
+            }
+        }
+        return aux;
+    }
+}
