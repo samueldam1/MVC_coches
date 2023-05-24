1b8d30c2326d14c476c5d2fca9dc08007d8c862b
Samuel Gardón Prieto <114077937+samueldam1@users.noreply.github.com>
2023-05-24 14:13:30 +0200

    Update View.java
    Añadido el método "mostrarBusqueda"

--- src/View.java
+++ src/View.java
@@ -3,4 +3,11 @@ public class View {
         System.out.println(matricula + ": " + v + "km/hr");
         return true;
     }
+
+    void muestraBusqueda(String modelo, String matricula, Integer v){
+        System.out.println("RESULTADO DE LA BUSQUEDA: "
+                +"\nModelo: "+modelo
+                +"\nMatricula: "+matricula
+                +"\nVelocidad: "+v);
+    }
 }
