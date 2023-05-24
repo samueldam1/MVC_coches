a3620b8255e6bb794addca157da85224a71a6fa2
Samuel Gardón Prieto <114077937+samueldam1@users.noreply.github.com>
2023-05-24 14:11:42 +0200

    Update Controller.java
    Cambios y añadido el método "buscarCoche"

--- src/Controller.java
+++ src/Controller.java
@@ -1,23 +1,35 @@
 public class Controller {
-    static Model miModelo = new Model();
-    static View miVista = new View();
+    static Model model = new Model();
+    static View view = new View();
     public static void main(String[] args) {
         IU.crearVentana();
     }
     public static void crearCoche(String modelo, String matricula){
-        Coche aux = miModelo.crearCoche(modelo,matricula);
+        Coche aux = model.crearCoche(modelo,matricula);
         if(aux!=null){
-            miVista.muestraVelocidad(aux.matricula, aux.velocidad);
+            view.muestraVelocidad(aux.matricula, aux.velocidad);
         }
     }
 
     public static void bajarVelocidad(String matricula){
-        int aux = miModelo.bajarVelocidad(matricula);
-        miVista.muestraVelocidad(matricula, aux);
+        int aux = model.bajarVelocidad(matricula);
+        view.muestraVelocidad(matricula, aux);
     }
 
     public static void aumentarVelocidad(String matricula){
-        int aux = miModelo.subirVelocidad(matricula);
-        miVista.muestraVelocidad(matricula,aux);
+        int aux = model.subirVelocidad(matricula);
+        view.muestraVelocidad(matricula,aux);
+    }
+
+    /**
+     * Metodo que a partir de una matricula devuelve el modelo y la velocidad
+     * del coche asociado a esa matricula
+     * @param matricula
+     */
+    public static void buscarCoche(String matricula){
+        String modelo = model.getModelo(matricula);
+        int v = model.getVelocidad(matricula);
+
+        view.muestraBusqueda(modelo,matricula,v);
     }
 }
