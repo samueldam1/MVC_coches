c67d4e05760deb57c8851595d5e96a23196c0bf3
Samuel Gardón Prieto <114077937+samueldam1@users.noreply.github.com>
2023-05-25 23:04:32 +0200

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,20 @@
+
+
 public class Controller {
+    static Model miModelo;
+
     public static void main(String[] args) {
-        Model miModelo = new Model();
-        View miVista = new View();
+
+        // el patron Observer en java nos exige instanciar la clase observable
+        miModelo = new Model();
+
+        // instanciamos al observador
+        ObserverVelocidad observoVelocidad = new ObserverVelocidad();
+        miModelo.addObserver(observoVelocidad);
+
+        // instanciamos un segundo observador
+        ObserverOtro otroObservador = new ObserverOtro();
+        miModelo.addObserver(otroObservador);
 
         // Crear tres coches
 
@@ -9,13 +22,17 @@ public class Controller {
         miModelo.crearCoche("Alpine", "HYU 4567");
         miModelo.crearCoche("Aston Martin", "FGH 3333");
 
-        Coche ferrari = miModelo.getCoche("SBC 1234");
+        Coche ferrari = Model.getCoche("SBC 1234");
         // modifica la velocidad
         miModelo.cambiarVelocidad("SBC 1234", 30);
 
-        // recoje la velocidad y la muestra (tarea de la View)
-        boolean hecho = miVista.muestraVelocidad("SBC 1234", miModelo.getVelocidad("SBC 1234"));
+        // otro cambio de velocidad
+        miModelo.cambiarVelocidad("HYU 4567", 100);
 
-        System.out.println(hecho);
+        // (ya no es necesario, lo hace el observador)
+        // recoje la velocidad y la muestra (tarea de la View)
+        // boolean hecho = View.muestraVelocidad("SBC 1234", Model.getVelocidad("SBC 1234"));
+        // System.out.println(hecho);
     }
+
 }
