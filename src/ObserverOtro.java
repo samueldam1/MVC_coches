40aa3ffd98de4be6db6a7ab0c2e9fbc2779910bf
Samuel Gardón Prieto <114077937+samueldam1@users.noreply.github.com>
2023-05-25 23:05:10 +0200

    Create ObserverOtro.java

---
+++ src/ObserverOtro.java
@@ -0,0 +1,19 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObserverOtro implements Observer {
+    /**
+     * This method is called whenever the observed object is changed. An
+     * application calls an {@code Observable} object's
+     * {@code notifyObservers} method to have all the object's
+     * observers notified of the change.
+     *
+     * @param o   the observable object.
+     * @param arg an argument passed to the {@code notifyObservers}
+     *            method.
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("Yo tambien me enteré que la velocidad cambio en " + arg.toString());
+    }
+}
